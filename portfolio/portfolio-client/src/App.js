38d34955f956ba9a7164d8e75257d512298e5618
Aitor Arina <aitorulz1@gmail.com>
2020-12-15 22:42:15 +0100

    Deleted ToysRused and using structure to begin Portfolio project

---
+++ portfolio/portfolio-client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Route} from 'react-router-dom';
+import { Switch } from 'react-router';
+
+import Home from './components/Main/Home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Home 
+      
+      />
+    </BrowserRouter>
+  );
+}
+
+export default App;
