a99d657dfa5d8de16542c726cb5a15da6367c8c4
Aitor Arina <aitorulz1@gmail.com>
2021-01-04 20:52:18 +0100

    updating pics and structure

--- portfolio/portfolio-client/src/components/Main/AllCategories.js
+++ portfolio/portfolio-client/src/components/Main/AllCategories.js
@@ -1,22 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import clienteAxios from '../../service/axios';
 
+
+
 export default function AllCategories() {
 
-    const [ categories, getCategories ] = useState([])
+    const [ categories, getCategories ] = useState([]);
 
-    useEffect(() => {
+    useEffect( async () => {
         try {
-            const respuesta = clienteAxios.get('/categories')
-            console.log(respuesta)
+            const respuesta = await clienteAxios.get('/categories');
+            getCategories(respuesta);
+            console.log(respuesta.data);
         } catch (error) {
-            
+         
         }
     })
 
     return (
         <div>
-            
+            {/* {categories.map(category => (
+                    <Menu {...category} />                    
+            ))} */}
         </div>
     )
 }
--- portfolio/portfolio-client/src/components/Main/Home.js
+++ portfolio/portfolio-client/src/components/Main/Home.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react';
+
 
 export default function Home() {
+
+    
+
     return (
         <div>
-            Hola
+            
         </div>
     )
 }
